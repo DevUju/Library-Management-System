21b6c4a4173db4907a730c308596365ec0df227b
Obianuju1998 <agnesanosike@gmail.com>
2024-08-25 15:31:49 +0100

    added environment variables to the config.py

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 # Set up the configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['CACHE_TYPE'] = 'RedisCache' 
-app.config['CACHE_REDIS_HOST'] = 'localhost'
-app.config['CACHE_REDIS_PORT'] = 6379
-app.config['CACHE_DEFAULT_TIMEOUT'] = 300
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+app.config["CACHE_TYPE"] = os.getenv("CACHE_TYPE") 
+app.config["CACHE_REDIS_HOST"] = os.getenv("CACHE_REDIS_HOST")
+app.config["CACHE_REDIS_PORT"] = os.getenv("CACHE_REDIS_PORT")
+app.config["CACHE_DEFAULT_TIMEOUT"] = os.getenv("CACHE_DEFAULT_TIMEOUT")
 
 # Initialize the database
 db = SQLAlchemy(app)
